fc9371862477e6847bd2e7e3f6c90f55e58ad94a
korgeron <orgeron.kevin@gmail.com>
2023-10-23 14:51:12 -0500

    tests

--- test_sample.py
+++ test_sample.py
@@ -2,18 +2,33 @@
     Define a function to test
 """
 
-
-def func(x):
-    """
-        This code will fail the test below. To pass must be x + 2
-    """
-    return x + 1
+# def func(x):
+#     """
+#         This code will fail the test below. To pass must be x + 2
+#     """
+#     return x + 1
 
 
 """
     Test the function above
 """
 
+# def test_func():
+#     assert func(3) == 5
 
-def test_func():
-    assert func(3) == 5
+
+"""
+    Group Multiple Tests in a Class!
+    Not recommended!!!
+"""
+
+# class TestClass:
+#     def test_one(self):
+#         x = "this"
+#         assert "h" in x
+#
+#     def test_two(self):
+#         x = "hello"
+#         assert hasattr(x, "hello")
+
+
