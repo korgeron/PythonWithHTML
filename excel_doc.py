d5b7f2f8db537adfe3b15e4bc733cb76b6298fc2
korgeron <orgeron.kevin@gmail.com>
2023-10-25 17:12:07 -0500

    finding what framework to use for excel documents

---
+++ excel_doc.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+"""
+    Dataframes are also known as Tables 
+"""
+df = pd.DataFrame(
+                {"x":           [1, 2, 3],
+                 "y":           [4, 5, 6]})
+print(df)
